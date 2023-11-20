e6ac59dae5dd6b8384d80ea5bf0859a4fbdfd19d
Audora <gy2553@naver.com>
2023-11-20 12:51:31 +0900

    second

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # 파일 경로 지정
 file_path = 'test.txt'
 
-# 파일 열기
+# # 파일 열기
 with open(file_path, 'r', encoding='utf-8') as file:
-    # 파일 내용 읽기
-    file_content = file.read()
+    # 파일 내용을 한 글자씩 읽어와서 출력
+    for char in file.read():
+        char=char+"a"
+        print(char)
 
-    # 파일 내용 출력
-    print(file_content)
-
+# 또는 다음과 같이 사용할 수도 있습니다.
+# with open(file_path, 'r', encoding='utf-8') as file:
+#     file_content = file.readlines()
+#     for line in file_content:
+#         print(line, end='')
